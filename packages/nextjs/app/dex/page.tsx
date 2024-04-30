ca58945fc613a3ae679dfb991db1e207a0689541
Sahil Kharya <kharyasahil@gmail.com>
2024-04-29 23:14:44 -0500

    Page Readme update

--- packages/nextjs/app/dex/page.tsx
+++ packages/nextjs/app/dex/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import { wrapInTryCatch } from "~~/utils/scaffold-eth/common";
 import axios from "axios";
-import { ethers } from "ethers";
+import { ethers, formatEther } from "ethers";
 import type { NextPage } from "next";
 import { Line } from "react-chartjs-2";
 import { useAccount } from "wagmi";
@@ -14,6 +14,8 @@ import {
   useScaffoldContractRead,
   useScaffoldContractWrite,
 } from "~~/hooks/scaffold-eth";
+import { CovalentClient } from "@covalenthq/client-sdk";
+import { Curve } from "~~/components/Curve";
 
 // REGEX for number inputs (only allow numbers and a single decimal point)
 const NUMBER_REGEX = /^\.?\d+\.?\d*$/;
@@ -42,20 +44,58 @@ const Dex: NextPage = () => {
   const [amountIn, setAmountIn] = useState(0);
   const [amountOut, setAmountOut] = useState(0);
   const [duration, setDuration] = useState("");
+  const [chartData, setChartData] = useState({});
+  const [ETH_price, setETH_price] = useState('');
+  let aa;
+  const Covalent_API = `https://api.covalenthq.com/v1/pricing/historical/USD/ETH/?key=` + process.env.NEXT_PUBLIC_COVALENT_KEY;
+  // console.log(Covalent_API)
+  const fetchPrices = async () => {
+    try {
+      const client = new CovalentClient("cqt_rQt9JtGvcX7D7468yrGW8YCrXfKK");
+      const resp = await client.PricingService.getTokenPrices("eth-mainnet", "USD", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", { "from": "2024-03-11", "to": "2024-04-29", "pricesAtAsc": false });
+      console.log(resp.data);
+      const prices = resp.data[0].items;
+      setChartData({
+        labels: prices.map(data => new Date(data.date).toLocaleDateString()),
+        datasets: [
+          {
+            label: 'ETH/USD Price',
+            data: prices.map(data => data.price),
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          }
+        ]
+      });
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
 
+  useEffect(() => {
+    fetchPrices();
+    aa = PriceFeed
+  }, []);
   const { writeAsync: createQuoteWrite } = useScaffoldContractWrite({
     contractName: "RollSwap",
     functionName: "createQuote",
-    args: [USDC_token, ETH_token, BigInt(amountIn), BigInt(amountOut), BigInt(duration)],
+    args: [
+      USDC_token,
+      ETH_token,
+      BigInt(amountIn),
+      BigInt(amountOut),
+      duration
+    ],
   });
-  const { data: p } = useScaffoldContractRead({
+  const { data: PriceFeed } = useScaffoldContractRead({
     contractName: "RollSwap",
     functionName: "getLatestPrice",
   });
+
+  const priceAsString = PriceFeed ? PriceFeed.toString() : 'Loading...';
+  const EthVal = Number(priceAsString) / 100000000
+
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     console.log(event);
-    const val = await p;
     const tx = await createQuoteWrite();
     console.log("Transaction:", tx);
   };
@@ -93,28 +133,51 @@ const Dex: NextPage = () => {
             <div className="py-3 px-4">
               <div className="flex mb-4 justify-center items-center">
                 <form onSubmit={handleSubmit}>
-                  <input value={amountIn} onChange={e => setAmountIn(Number(e.target.value))} placeholder="Amount In" />
-                  <input
-                    value={amountOut}
-                    onChange={e => setAmountOut(Number(e.target.value))}
-                    placeholder="Amount Out"
-                  />
-                  <input value={duration} onChange={e => setDuration(e.target.value)} placeholder="Duration" />
-                  <button type="submit">Create Quote</button>
+                  <div className="mb-3">
+                    <label htmlFor="amountIn" className="block mb-2">Eth To Swap:</label>
+                    <input
+                      id="amountIn"
+                      type="number" // assuming input should be restricted to numbers
+                      value={amountIn}
+                      onChange={e => setAmountIn(Number(e.target.value))}
+                      style={{ borderRadius: '8px', backgroundColor: 'rgba(209, 235, 254, 0.5)', padding: '3px' }}
+                      placeholder="Enter amount in"
+                      className="form-input"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="amountOut" className="block mb-2">Eth/USDC Rate:</label>
+                    <input
+                      id="amountOut"
+                      type="number" // assuming input should be restricted to numbers
+                      value={amountOut}
+                      onChange={e => setAmountOut(Number(e.target.value))}
+                      style={{ borderRadius: '8px', backgroundColor: 'rgba(209, 235, 254, 0.5)', padding: '3px' }}
+                      placeholder="Enter amount out"
+                      className="form-input"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="duration" className="block mb-2">Duration:</label>
+                    <input
+                      id="duration"
+                      type="text"
+                      value={duration}
+                      onChange={e => setDuration(e.target.value)}
+                      style={{ borderRadius: '8px', backgroundColor: 'rgba(209, 235, 254, 0.5)', padding: '6px' }}
+                      placeholder="Duration"
+                      className="form-input"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary">Create Quote</button>
                 </form>
               </div>
+
               <div className="flex justify-center items-center">
                 <span className="w-1/2">
-                  tokenToETH{" "}
-                  <IntegerInput
-                    value={tokenToETHAmount}
-                    onChange={value => {
-                      setEthToTokenAmount("");
-                      setTokenToETHAmount(value.toString());
-                    }}
-                    name="tokenToETH"
-                    disableMultiplyBy1e18
-                  />
+                  <p className="text-center text-primary-content text-xl mt-8 -ml-8">
+                    Current ETH/USD : {EthVal}
+                  </p>
                 </span>
                 {/* <button
                   className="btn btn-primary h-[2.2rem] min-h-[2.2rem] mt-6 mx-5"
@@ -131,11 +194,13 @@ const Dex: NextPage = () => {
           {/* <Curve
             addingEth={ethToTokenAmount !== "" ? parseFloat(ethToTokenAmount.toString()) : 0}
             addingToken={tokenToETHAmount !== "" ? parseFloat(tokenToETHAmount.toString()) : 0}
-            ethReserve={contractETHBalance ? parseFloat("" + contractETHBalance) : 0}
-            tokenReserve={parseFloat(formatEther(contractBalance || 0n))}
+            USD={contractETHBalance ? parseFloat("" + 3320) : 0}
+            ETH={parseFloat(formatEther(222 || 0n))}
             width={500}
             height={500}
           /> */}
+          {/* <h2>ETH/USD Historical Price Chart</h2>
+          <Line data={chartData} /> */}
         </div>
       </div>
     </>
